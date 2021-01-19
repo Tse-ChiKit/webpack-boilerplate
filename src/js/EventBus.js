56e63014531cf3e13a6f1842c22f7ce7570ba681
Eric Xie <397706572@qq.com>
2021-01-19 21:39:01 +0800

    eventbus

--- src/js/EventBus.js
+++ src/js/EventBus.js
@@ -26,13 +26,14 @@ EventBus.prototype.emit = function (type, ...args ){
 
 EventBus.prototype.off = function (type, cb ){
     if(this.listeners[type] ){
-        return false;
-    }
-    else{
-        let fns = this.listeners[type];
-        fns.forEach(cb => {
-            cb(...args);
-        });
+       const curIndex = this.listeners[type].findIndex(it => it === cb)
+       if( curIndex >= 0){
+           this.listeners[type].splice(curIndex, 1);
+       }
+
+       if(this.listeners[type].length === 0){
+           delete this.listeners[type];
+       }
     }
     
 }
